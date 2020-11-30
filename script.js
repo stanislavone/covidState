04b8ef83dd292ce7a451855b1676a7986ffabd89
Glitch (quirky-necessary-kitchen) <none>
2020-11-30 18:18:00 +0000

    🔨🌻 Updated with Glitch

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  $.ajax({
+    url:
+      "https://covid-api.com/api/reports?date=" +
+      data() +
+      "&iso=RUS&region_province=Sverdlovsk%20Oblast",
+    success: function(data) {
+      console.log(data.data[0]);
+      $(".newConf").text(data.data[0].confirmed_diff);
+      $(".сonf").text(data.data[0].confirmed);
+      $(".active").text(data.data[0].active);
+      $(".lastUpdate").text(data.data[0].last_update);
+      $(".death").text(data.data[0].deaths_diff);
+      $(".deathDiff").text(data.data[0].deaths);
+      $(".recovery").text(data.data[0].recovered_diff);
+      $(".recoveryDiff").text(data.data[0].recovered);
+      $(".region").text(data.data[0]["region"].province)
+    }
+  });
+
+  function data() {
+    let now = new Date();
+    let mon = now.getMonth() + 1;
+    let day = now.getFullYear() + "-" + mon + "-" + (now.getDate() - 1);
+    return day;
+  }
+});
